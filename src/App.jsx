03285473db0a7f4e85b009d0f73ca7e1430ed522
Kevskiiis <delword07@gmail.com>
2025-06-21 23:45:59 -0700

    Remove .env from version control

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from 'react'
+import './styles/App.css';
+
+//Components:
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import CreateArea from './components/CreateArea.jsx';
+import Note from './components/Note.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [notes, setNotes] = useState([]); 
+
+  function addNote (note) {
+    setNotes ((prevValue) => {
+      return [...prevValue, note]
+    }); 
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header/>
+      <CreateArea addNote={addNote}/>
+      {notes.map((note, index) => {
+        return <Note key={index} title={note.title} content={note.content }/>
+      })};
+      <Note key={1} title="Note title" content="Note content"/>
+      <Footer/>
     </>
   )
 }
-
 export default App
